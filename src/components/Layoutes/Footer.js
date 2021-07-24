a367ecd7816a15c338531628c1ca9b20713a2394
mehedi hasan <mehedihasanibnc@gmail.com>
2021-07-24 16:56:49 +0600

    create home page and dynamic titles

---
+++ src/components/Layoutes/Footer.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+const Footer = () => {
+    return (
+        <Fragment>
+            <footer className="">
+                <p className="text-center text-white bg-dark p-3 m-0">
+                    eCommerce website - 2021-2022, All Rights Reserved
+                </p>
+            </footer>
+        </Fragment>
+    );
+};
+
+export default Footer;
