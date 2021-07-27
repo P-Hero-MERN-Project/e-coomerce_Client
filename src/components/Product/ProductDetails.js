920f137bd9e8b47556cbd1df13e8c776e8eeda53
mehedi hasan <mehedihasanibnc@gmail.com>
2021-07-27 10:28:01 +0600

    make redux add to cart

--- src/components/Product/ProductDetails.js
+++ src/components/Product/ProductDetails.js
@@ -1,12 +1,15 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { useAlert } from 'react-alert';
 import { Carousel } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearErrors, getProductDetails } from '../../action/productAction';
 import Loader from '../Layoutes/Loader';
 import MetaData from './../Layoutes/MetaData';
+import { addItemToCart } from './../../action/cartAction';
 
 const ProductDetails = ({ match }) => {
+
+    const [quantity, setQuantity] = useState(1)
 
     const alert = useAlert()
 
@@ -23,6 +26,34 @@ const ProductDetails = ({ match }) => {
         }
 
     }, [dispatch, alert, error, match.params.id])
+
+    const increaseQty = () => {
+
+        const count = document.querySelector('.count')
+
+        if(count.valueAsNumber >= product.stock) return;
+
+        const qty = count.valueAsNumber + 1
+        
+        setQuantity(qty)
+
+    }
+
+    const decreaseQty = () => {
+
+        const count = document.querySelector('.count')
+
+        if( count.valueAsNumber <= 1 ) return;
+
+        const qty = count.valueAsNumber - 1
+        
+        setQuantity(qty)
+    }
+
+    const addToCart = () => {
+        dispatch(addItemToCart(match.params.id, quantity))
+        alert.success('Item added successfully')
+    }
 
     return (
         <Fragment>
@@ -55,13 +86,15 @@ const ProductDetails = ({ match }) => {
 
                             <p id="product_price">${product.price}</p>
                             <div className="stockCounter d-inline">
-                                <span className="btn btn-danger minus">-</span>
+                                <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
 
-                                <input type="number" className="form-control count d-inline" value="1" readOnly />
+                                <input type="number" className="form-control count d-inline" value={quantity} readOnly />
 
-                                <span className="btn btn-primary plus">+</span>
+                                <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
                             </div>
-                            <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4">Add to Cart</button>
+                            <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4" disabled={product.stock === 0} onClick={addToCart}>
+                                Add to Cart
+                            </button>
 
                             <hr />
 
--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 import { productReducer, productDetailsReducer, newProductReducer, deleteProductReducer } from '../src/Reducers/productReducer'
 import { authReducer, userReducer } from '../src/Reducers/userReducer'
+import { cartReducers } from "./Reducers/cartReducer";
 
 const reducer = combineReducers({
     products: productReducer,
@@ -11,10 +12,15 @@ const reducer = combineReducers({
     auth: authReducer,
     user: userReducer,
     newProduct: newProductReducer,
-    product: deleteProductReducer
+    product: deleteProductReducer,
+    cart: cartReducers
 })
 
-let initialState = {}
+let initialState = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+    }
+}
 
 const middleware = [thunk]
 
